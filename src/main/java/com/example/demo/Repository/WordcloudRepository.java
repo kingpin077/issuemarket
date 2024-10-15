0b2684a8ea27cd99fda539b076fc772da2c55be3
PC <mokjong987@naver.com>
2024-10-15 21:16:16 +0900

    1. filter테이블 이용해서 사용자가 클릭,검색한 데이터만을 가지고 워드클라우드 추가

--- src/main/java/com/example/demo/Repository/WordcloudRepository.java
+++ src/main/java/com/example/demo/Repository/WordcloudRepository.java
@@ -10,6 +10,19 @@ import java.util.List;
 
 @Repository
 public interface WordcloudRepository extends JpaRepository<Serch_infoEntity, Long> {
+    //전체사용자 데이터
     @Query("SELECT f.keyword, COUNT(f.keyword) AS count FROM Serch_infoEntity f GROUP BY f.keyword ORDER BY count(f.keyword) DESC")
     List<Object[]> findKeywordCount();
+
+    //로그인 사용자 데이터
+    @Query("SELECT f.keyword, COUNT(f.keyword) AS count FROM Serch_infoEntity f WHERE f.userId <> 'anonymousUser'  GROUP BY f.keyword ORDER BY count(f.keyword) DESC")
+    List<Object[]> loginuser();
+
+    //여성사용자 데이터
+    @Query("SELECT f.keyword, COUNT(f.keyword) AS count FROM Serch_infoEntity f WHERE f.gender = false  GROUP BY f.keyword ORDER BY count(f.keyword) DESC")
+    List<Object[]> femaleuser();
+
+    //남성사용자 데이터
+    @Query("SELECT f.keyword, COUNT(f.keyword) AS count FROM Serch_infoEntity f WHERE f.gender = true  GROUP BY f.keyword ORDER BY count(f.keyword) DESC")
+    List<Object[]> maleuser();
 }
