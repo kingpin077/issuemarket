54aea48053e93aa39a205efb52fea6658159d602
PC <mokjong987@naver.com>
2024-11-24 15:44:38 +0900

    다국어처리

--- src/main/java/com/example/demo/MessageConfig.java
+++ src/main/java/com/example/demo/MessageConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
 
 @Configuration
 public class MessageConfig {
@@ -18,21 +14,5 @@ public class MessageConfig {
         messageSource.setBasename("classpath:messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
-    }
-
-    // LocaleResolver 빈 등록
-    @Bean
-    public SessionLocaleResolver localeResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
-        localeResolver.setDefaultLocale(Locale.KOREAN); // 기본 언어 설정 (한국어)
-        return localeResolver;
-    }
-
-    // Locale 변경을 위한 인터셉터 등록
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
-        interceptor.setParamName("lang"); // URL 파라미터를 통해 언어 설정 변경
-        return interceptor;
     }
 }
